558ec86983b73c31aa3120a388b352aab6d49119
Abdurazaq333 <irazaqr986@gmail.com>
2025-07-29 13:48:50 +0500

    working with card props

--- src/components/Dashboard/Card.jsx
+++ src/components/Dashboard/Card.jsx
@@ -1,33 +1,42 @@
 import React from "react";
-import { FaDollarSign } from "react-icons/fa6";
 
-const Card = () => {
+const Card = ({
+  title,
+  total,
+  name,
+  icon0,
+  icon1,
+  nTotal,
+  icon2,
+  grp,
+  complete,
+}) => {
   return (
     <div className="w-full  p-4 bg-blue-900 rounded-md text-white">
       <div>
-        <h2 className="text-2xl ">user</h2>
+        <h2 className="text-2xl ">{title}</h2>
         <div className="flex justify-between mt-4 items-center">
           <div>
-            <h2 className="text-3xl font-bold">7877</h2>
-            <h3>Total Order</h3>
+            <h2 className="text-3xl font-bold">{total}</h2>
+            <h3>{name}</h3>
           </div>
-          <div className="bg-blue-600 rounded-full p-2">
-            <FaDollarSign size={25} />
-          </div>
+          <div className="bg-blue-600 rounded-full p-2">{icon0}</div>
         </div>
         <input
           type="range"
           min="0"
           max="100"
-          value={75}
+          value={complete}
           className="w-full my-4"
         />
         <div className="flex justify-between">
           <div className="flex gap-1 items-center text-sm">
-            <FaDollarSign /> <span>Profit: $8,245</span>
+            {icon1} <span>{nTotal}</span>
           </div>
           <div className="flex gap-1 items-center text-sm">
-            <FaDollarSign /> <span>Goal: 75%</span>
+            {icon2}
+            <span> {grp}:</span>
+            <span> {complete}%</span>
           </div>
         </div>
       </div>
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Card from "./Card";
 
+import { FaDollarSign } from "react-icons/fa6";
+import { FaUsers, FaUserPlus } from "react-icons/fa";
+import { MdNoteAlt } from "react-icons/md";
+import { IoCheckmarkDone, IoTimeOutline } from "react-icons/io5";
+import { GoGoal } from "react-icons/go";
+import { FaArrowsRotate } from "react-icons/fa6";
+
 const Dashboard = () => {
+  const SoTotal = 24580;
+  const SoObtain = 8245;
+  let SoComp = ((SoObtain / SoTotal) * 100).toFixed(2);
+
+  const UeTotal = 8360;
+  const UeObtain = 348;
+  let UeComp = ((UeObtain / UeTotal) * 100).toFixed(2);
+
+  // Order Status
+  const OsTotal = 1245;
+  const OsObtain = 936;
+  let OsComp = ((OsObtain / OsTotal) * 100).toFixed(2);
+
   return (
     <div className="mt-18 bg-[#161676] w-full">
       <div className="p-10 w-full">
@@ -14,11 +35,41 @@ const Dashboard = () => {
         </div>
         <div className="flex flex-col mt-4 gap-4">
           <div className="flex  justify-around gap-4 ">
-            <Card />
-            <Card />
+            <Card
+              title={"Sales Overview"}
+              total={SoTotal.toLocaleString()}
+              name={"Total monthly revenue"}
+              icon0={<FaDollarSign size={25} />}
+              icon1={<FaDollarSign />}
+              nTotal={"Profit: " + SoObtain}
+              icon2={<GoGoal />}
+              grp={"Goal"}
+              complete={SoComp}
+            />
+            <Card
+              title={"User Engagement"}
+              total={UeTotal.toLocaleString()}
+              name={"Active users"}
+              icon0={<FaUsers size={25} />}
+              icon1={<FaUserPlus />}
+              nTotal={"New: " + UeObtain}
+              icon2={<FaArrowsRotate />}
+              grp={"Returning:"}
+              complete={UeComp}
+            />
           </div>
           <div>
-            <Card />
+            <Card
+              title={"Order Status"}
+              total={OsTotal}
+              name={"Total orders today"}
+              icon0={<MdNoteAlt size={25} />}
+              icon1={<IoCheckmarkDone />}
+              nTotal={"Completed: " + OsObtain}
+              icon2={<IoTimeOutline />}
+              grp={"Pending:"}
+              complete={OsComp}
+            />
           </div>
         </div>
       </div>
